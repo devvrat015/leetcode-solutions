8e5118a1369e10b59ff20d09f8c27c19e922bcae
Devvrat Kokane <157376658+devvrat015@users.noreply.github.com>
2026-09-15 20:43:34 +0530

    feat: add 0001. Two Sum [Easy]

---
+++ topics/array/0001_two_sum.py
@@ -0,0 +1,12 @@
+"""
+Problem    : 0001. Two Sum
+Link       : https://leetcode.com/problems/two-sum/
+Difficulty : Easy
+Tags       : Array, Hash Table
+Runtime    : 52 ms (beats 87.5%)
+Memory     : 14.3 MB (beats 72.1%)
+"""
+
+class Solution:
+    def twoSum(self, nums, target):
+        return []
