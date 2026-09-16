f3e3141ffe0bc1bf490826c7e8e87caf5e3c2882
Devvrat Kokane <157376658+devvrat015@users.noreply.github.com>
2026-09-16 13:33:12 +0530

    chore: update 0002. Add Two Numbers [Medium]

--- topics/linked_list/0002_add_two_numbers.py
+++ topics/linked_list/0002_add_two_numbers.py
@@ -3,15 +3,22 @@ Problem    : 0002. Add Two Numbers
 Link       : https://leetcode.com/problems/add-two-numbers/
 Difficulty : Medium
 Tags       : Linked List, Math, Recursion
-Runtime    : 0 ms (beats 0.0%)
-Memory     : 0.0 MB (beats 0.0%)
+Runtime    : 0 ms (beats 24.45%)
+Memory     : 19.17 MB (beats 96.04%)
 """
 
 
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        n = len(nums)
-        for i in range(n):
-            for j in range(i + 1, n):
-                if nums[i] + nums[j] == target:
-                    return ([i, j])
+    def addTwoNumbers(self, l1: ListNode | None, l2: ListNode | None) -> ListNode | None:
+        dummy = ListNode(0)
+        current = dummy
+        carry = 0
+
+        while l1 or l2 or carry:
+            val1 = l1.val if l1 else 0
+            val2 = l2.val if l2 else 0
