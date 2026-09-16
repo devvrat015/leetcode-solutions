df2b0672f6ae34830b9dc0275bb997b378c0317a
Devvrat Kokane <157376658+devvrat015@users.noreply.github.com>
2026-09-16 13:19:13 +0530

    chore: update 0001. Two Sum [Easy]

--- topics/array/0001_two_sum.py
+++ topics/array/0001_two_sum.py
@@ -3,10 +3,16 @@ Problem    : 0001. Two Sum
 Link       : https://leetcode.com/problems/two-sum/
 Difficulty : Easy
 Tags       : Array, Hash Table
-Runtime    : 52 ms (beats 87.5%)
-Memory     : 14.3 MB (beats 72.1%)
+Runtime    : 0 ms (beats 0.0%)
+Memory     : 0.0 MB (beats 0.0%)
 """
 
+
 class Solution:
-    def twoSum(self, nums, target):
-        return []
+    def twoSum(self, nums: List[int], target:
+    int) -> List[int]:
+        n = len(nums)
+        for i in range(n):
+            for j in range(i + 1, n):
+                if nums[i] + nums[j] == target:
+                    return ([i, j])
