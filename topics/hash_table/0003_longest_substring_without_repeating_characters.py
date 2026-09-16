5f62593ddbe7b1d055b5a3700e6accc21c8d4c47
Devvrat Kokane <157376658+devvrat015@users.noreply.github.com>
2026-09-16 14:04:14 +0530

    chore: update 0003. Longest Substring Without Repeating Characters [Medium]

--- topics/hash_table/0003_longest_substring_without_repeating_characters.py
+++ topics/hash_table/0003_longest_substring_without_repeating_characters.py
@@ -9,7 +9,8 @@ Memory     : 19.85 MB (beats 60.24%)
 
 
 class Solution:
-    def lengthOfLongestSubstring(self, s: str) -> int:
+    def lengthOfLongestSubstring(self, s:
+    str) -> int:
         left = 0
         max_length = 0
         seen = set()
@@ -20,6 +21,7 @@ class Solution:
                 left += 1
 
             seen.add(s[right])
-            max_length = max(max_length, right - left + 1)
+            max_length = max(max_length,
+            right - left + 1)
 
         return max_length
