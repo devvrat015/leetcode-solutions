3d2e885896e27a608835e5cd370ee26eb4c888dd
Devvrat Kokane <157376658+devvrat015@users.noreply.github.com>
2026-09-16 13:54:46 +0530

    chore: update 0003. Longest Substring Without Repeating Characters [Medium]

--- topics/hash_table/0003_longest_substring_without_repeating_characters.py
+++ topics/hash_table/0003_longest_substring_without_repeating_characters.py
@@ -3,12 +3,9 @@ Problem    : 0003. Longest Substring Without Repeating Characters
 Link       : https://leetcode.com/problems/longest-substring-without-repeating-characters/
 Difficulty : Medium
 Tags       : Hash Table, String, Sliding Window
-Runtime    : 2 ms (beats 21.96%)
-Memory     : 19.9 MB (beats 60.24%)
+Runtime    : 215 ms (beats 38.12%)
+Memory     : 19.98 MB (beats 29.59%)
 """
 
 
 class Solution:
-    def lengthOfLongestSubstring(self, s: str) -> int:
-        left = 0
-        max_length = 0
