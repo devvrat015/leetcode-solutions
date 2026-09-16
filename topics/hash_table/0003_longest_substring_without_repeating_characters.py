6ca3d640ab2874ec4b7d42026e9166dc3efe37d7
Devvrat Kokane <157376658+devvrat015@users.noreply.github.com>
2026-09-16 14:02:06 +0530

    chore: update 0003. Longest Substring Without Repeating Characters [Medium]

--- topics/hash_table/0003_longest_substring_without_repeating_characters.py
+++ topics/hash_table/0003_longest_substring_without_repeating_characters.py
@@ -3,8 +3,8 @@ Problem    : 0003. Longest Substring Without Repeating Characters
 Link       : https://leetcode.com/problems/longest-substring-without-repeating-characters/
 Difficulty : Medium
 Tags       : Hash Table, String, Sliding Window
-Runtime    : 218 ms (beats 33.49%)
-Memory     : 19.95 MB (beats 29.59%)
+Runtime    : 186 ms (beats 67.21%)
+Memory     : 20.07 MB (beats 9.61%)
 """
 
 
@@ -14,3 +14,11 @@ class Solution:
         left = 0
         max_length = 0
         seen = set()
+
+        for right in range(len(s)):
+            while s[right] in seen:
+                seen.remove(s[left])
+                left += 1
+
+
+        return max_length
