8fad743a46a67436c7d0a0e7845a7cdf1f42bf67
Devvrat Kokane <157376658+devvrat015@users.noreply.github.com>
2026-09-21 19:58:55 +0530

    chore: update 0283. Move Zeroes [Easy]

--- topics/array/0283_move_zeroes.py
+++ topics/array/0283_move_zeroes.py
@@ -3,22 +3,15 @@ Problem    : 0283. Move Zeroes
 Link       : https://leetcode.com/problems/move-zeroes/
 Difficulty : Easy
 Tags       : Array, Two Pointers
-Runtime    : 0 ms (beats 22.73%)
-Memory     : 20.4 MB (beats 90.2%)
+Runtime    : 0 ms (beats 17.48%)
+Memory     : 20.54 MB (beats 25.81%)
 """
 
 class Solution:
     def moveZeroes(self, nums: list[int]) -> None:
         i = 0
-        j = 1
-        temp = 0
-        while j < len(nums):
-            if nums[i] != 0:
-                i+=1
-            if nums[i] != nums[j]:
-                temp =  nums[i]
-                nums[i] = nums[j]
-                nums[j] = temp
+
+        for j in range(len(nums)):
+            if nums[j] != 0:
+                nums[i], nums[j] = nums[j], nums[i]
                 i += 1
-            j+=1
-        return nums
