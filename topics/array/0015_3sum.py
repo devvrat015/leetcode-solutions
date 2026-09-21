c90c0a410a71edbaf53540f1dce380bbf553f1f5
Devvrat Kokane <157376658+devvrat015@users.noreply.github.com>
2026-09-21 21:16:40 +0530

    feat: add 0015. 3Sum [Medium]

---
+++ topics/array/0015_3sum.py
@@ -0,0 +1,47 @@
+"""
+Problem    : 0015. 3Sum
+Link       : https://leetcode.com/problems/3sum/
+Difficulty : Medium
+Tags       : Array, Two Pointers, Sorting
+Runtime    : 0 ms (beats 75.88%)
+Memory     : 22.41 MB (beats 18.91%)
+"""
+
+class Solution:
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
+        nums.sort()
+        result = []
+
+        for i in range(len(nums)):
+
+            # Skip duplicate values for i
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+
+            left = i + 1
+            right = len(nums) - 1
+
+            while left < right:
+                total = nums[i] + nums[left] + nums[right]
+
+                if total == 0:
+                    result.append([nums[i], nums[left], nums[right]])
+
+                    left += 1
+                    right -= 1
+
+                    # Skip duplicate left values
+                    while left < right and nums[left] == nums[left - 1]:
+                        left += 1
+
+                    # Skip duplicate right values
+                    while left < right and nums[right] == nums[right + 1]:
+                        right -= 1
+
+                elif total < 0:
+                    left += 1
+
+                else:
+                    right -= 1
+
+        return result
