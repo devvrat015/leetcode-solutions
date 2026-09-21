8d76d0391cf1bff1c91b1fb40157c63fdc4ad575
Devvrat Kokane <157376658+devvrat015@users.noreply.github.com>
2026-09-21 19:33:00 +0530

    chore: update 0026. Remove Duplicates from Sorted Array [Easy]

--- topics/array/0026_remove_duplicates_from_sorted_array.py
+++ topics/array/0026_remove_duplicates_from_sorted_array.py
@@ -3,19 +3,19 @@ Problem    : 0026. Remove Duplicates from Sorted Array
 Link       : https://leetcode.com/problems/remove-duplicates-from-sorted-array/
 Difficulty : Easy
 Tags       : Array, Two Pointers
-Runtime    : 0 ms (beats 100.0%)
-Memory     : 22.5 MB (beats 80.32%)
+Runtime    : 0 ms (beats 12.57%)
+Memory     : 20.57 MB (beats 43.63%)
 """
 
-class Solution {
-public:
-    int removeDuplicates(vector<int>& arr) {
-       int i = 0;
-    for(int j = 1; j < arr.size(); j++) {
-        if(arr[j] != arr[i]) {
-            arr[++i] = arr[j];
-        }
-    }
-    return i + 1;
- }
-};
+class Solution:
+    def removeDuplicates(self, nums: list[int]) -> int:
+
+        i = 0
+        j = 1
+
+        while j < len(nums):
+            if nums[i] != nums[j]:
+                nums[i+1] = nums[j]
+                i += 1
+            j+=1
+        return i+1
