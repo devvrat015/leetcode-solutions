751d9753fa317e6ca9444ecbdd0d2d703fc4e5c1
Devvrat Kokane <157376658+devvrat015@users.noreply.github.com>
2026-09-22 17:52:21 +0530

    chore: update 0001. Two Sum [Easy]

--- topics/array/0001_two_sum.py
+++ topics/array/0001_two_sum.py
@@ -3,15 +3,37 @@ Problem    : 0001. Two Sum
 Link       : https://leetcode.com/problems/two-sum/
 Difficulty : Easy
 Tags       : Array, Hash Table
-Runtime    : 0 ms (beats 0.0%)
-Memory     : 0.0 MB (beats 0.0%)
+Runtime    : 0 ms (beats 41.03%)
+Memory     : 20.75 MB (beats 7.51%)
 """
-
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        n = len(nums)
-        for i in range(n):
-            for j in range(i + 1, n):
-                if nums[i] + nums[j] == target:
-                    return ([i, j])
+        # two pointer approach
+        nums = [(num, i) for i, num in enumerate(nums)]
+        nums.sort()
+
+        start = 0
+        end = len(nums) - 1
+
+        while start < end:
+            current = nums[start][0] + nums[end][0]
+
+            if current == target:
+                return [nums[start][1], nums[end][1]]
+
+            elif current > target:
+                end -= 1
+
+            else:
+                start += 1
+            
+
+# hashmap approach
+            # hm = {}
+
+            # for i, n in enumerate(nums):
+            #     diff = target - n
+            #     if diff in hm:
+            #         return (hm[diff], i)
+            #     hm[n] = i
